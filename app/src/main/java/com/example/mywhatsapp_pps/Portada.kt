89213bdb230e3af3b892a014195ae6f8c4c4cf76
Pau1047 <alupin10474811@ieselcaminas.org>
2023-11-07 20:43:06 +0100

    4

--- app/src/main/java/com/example/mywhatsapp_pps/Portada.kt
+++ app/src/main/java/com/example/mywhatsapp_pps/Portada.kt
@@ -6,8 +6,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Search
@@ -24,6 +28,8 @@ import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -32,14 +38,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.example.mywhatsapp_pps.ui.theme.VerdeWas
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Portada(){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
 
-Scaffold(topBar = { MyTop()}) {
+Scaffold(modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),topBar = { MyTop(scrollBehavior)}){
     Box(
 
         modifier = Modifier
@@ -47,18 +57,25 @@ Scaffold(topBar = { MyTop()}) {
             .padding(top = it.calculateTopPadding())
     ){
         MyTabs()
+        LazyColumn{
+            items(getInfoWas()){ info ->
+                Mycard(infoWas = info)
+
+            }
+        }
+
         Box(modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.BottomEnd
         ){
             MyFAB()
         }
     }
-}
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTop(){
+fun MyTop(scrollBehavior: TopAppBarScrollBehavior){
     TopAppBar(
         colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = VerdeWas),
         title = { Text(text = "MyWhastApp", color = Color.White)  },
@@ -75,7 +92,8 @@ fun MyTop(){
                     contentDescription = "Share",
                     tint = Color.White)
             }
-        }
+        },
+        scrollBehavior = scrollBehavior
     )
 }
 
@@ -103,7 +121,68 @@ fun MyFAB(){
 }
 
 @Composable
-fun Mycard(){
+fun Mycard(infoWas: infoWas){
+   Card (){
+       Row {
+           Image(painterResource(id = infoWas.imagen), contentDescription =" ",
+               modifier = Modifier
+                   .width(70.dp)
+                   .height(70.dp))
 
+           Text(text = infoWas.name)
+       }
+   }
+   
 
+}
+
+data class infoWas(
+    var imagen: Int,
+    var name: String
+)
+
+fun getInfoWas(): List<infoWas>{
+    return listOf(
+        infoWas(
+            R.drawable.photo,
+            "Jefe de departamento"
+        ),
+        infoWas(
+            R.drawable.photo1,
+            "Tutora DAM"
+        ),
+        infoWas(
+            R.drawable.photo2,
+            "Tutor DAW"
+        ),
+        infoWas(
+            R.drawable.photo3,
+            "Tutora ASIX"
+        ),
+        infoWas(
+            R.drawable.photo4,
+            "Presidenta"
+        ),
+        infoWas(
+            R.drawable.photo5,
+            "Secretaria"
+        ),
+        infoWas(
+            R.drawable.photo6,
+            "Administrador"
+        ),
+        infoWas(
+            R.drawable.photo7,
+            "Entrenadora"
+        ),
+        infoWas(
+            R.drawable.photo8,
+            "Nutricionista"
+        ),
+        infoWas(
+            R.drawable.photo9,
+            "Fisioterapeuta"
+        )
+
+    )
 }
